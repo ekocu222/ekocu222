403d771de379a4f62341451ea3ac6dd7bb3d2860
ekocu222 <ekocu222@gmail.com>
2026-01-16 16:46:59 +0300

    Delete src/coach/dto/coach.module.ts

--- src/coach/dto/coach.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CoachService } from './coach.service';
-import { CoachController } from './coach.controller';
-
-@Module({
-  providers: [CoachService],
-  controllers: [CoachController],
-})
-export class CoachModule {}
